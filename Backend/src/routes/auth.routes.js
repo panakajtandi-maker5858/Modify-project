b6c832fff02a474aae33cf096e5fcabcd8fef3ba
panakajtandi-maker5858 <panakajtandi@gmail.com>
2026-04-16 15:22:23 +0530

    Modify authentication & Token Blacklistiong done using Redis

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,20 @@
+const { Router } = require("express")
+const authController = require("../controllers/auth.controller")
+const authUser = require("../middlewares/auth.middleware")
+
+
+const router = Router()
+
+
+
+router.post('/register' , authController.registerUser)
+
+
+router.post('/login' , authController.loginUser)
+
+router.get('/get-me' ,authUser, authController.getMe)
+
+router.get('/logout' , authController.logoutUser)
+
+
+module.exports = router ;
